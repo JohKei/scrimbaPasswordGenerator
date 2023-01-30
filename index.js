059fd24f0f1c561248e82402805bbc843dfa4fcc
John <kaebjohann@googlemail.com>
2023-01-30 19:50:16 +0100

    add function to include Numbers or/and Symbols

--- index.js
+++ index.js
@@ -1,21 +1,34 @@
 
 const characters =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P",
 "Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k",
-"l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4",
-"5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+",
-"=","{","[","}","]",",","|",":",";","<",">",".","?","/"
-];
+"l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",
+]
+const numbers = [ "0", "1", "2", "3", "4",
+"5", "6", "7", "8", "9",]
+
+const symbols = ["~","`","!","@","#","$","%","^","&","*","(",")","_","-","+",
+"=","{","[","}","]",",","|",":",";","<",">",".","?","/"]
+let addNumbers =document.getElementById('add-numbers')
+let addSymbols =document.getElementById('add-symbols')
 
 let characterCount=15
+
 function generatePassword(){
     let passwordOne=""
     let passwordTwo=""
-    let character= characters[Math.floor(Math.random()*characters.length)]
+    let merged=characters
+    if (addNumbers.checked){
+        merged = characters.concat(numbers)
+    }  
+    if (addSymbols.checked){
+        merged = merged.concat(symbols)
+    }
+    let character= merged[Math.floor(Math.random()*merged.length)]
     for (i=0; i<characterCount; i++){        
-        passwordOne += characters[Math.floor(Math.random()*characters.length)]
+        passwordOne += merged[Math.floor(Math.random()*merged.length)]
     }
     for (i=0; i<characterCount; i++){        
-        passwordTwo += characters[Math.floor(Math.random()*characters.length)]
+        passwordTwo += merged[Math.floor(Math.random()*merged.length)]
     }
     document.getElementById("passwordOne").textContent=passwordOne
     document.getElementById("passwordTwo").textContent=passwordTwo
